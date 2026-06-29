ceee6a9f9225533eb84294f02d1dc438316e746a
Deepthi <deepthi121416@gmail.com>
2026-06-29 08:21:15 +0530

    terraform-infra

---
+++ main.tf
@@ -0,0 +1,19 @@
+resource "aws_security_group" "main" {
+  name        = var.sg_name  
+  description = "Allow TLS inbound traffic and all outbound traffic"
+  vpc_id      = var.vpc_id
+
+   egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    
+  }
+
+  tags = {
+    Name = "${local.common_name}-${var.sg_name}"
+    project = var.project
+    environment = var.environment
+  }
+}
